4ab9000535b8c22935fc97e5a207c12261385e1e
CD Rivers <riverscd90@gmail.com>
2021-11-03 05:18:50 -0400

    added notes with functions I could think of. Once I figure out how to do the console/node thing to see if I'm doing it right, I will take notes off.

--- src/script.js
+++ src/script.js
@@ -6,4 +6,40 @@ function randomDamage(){
     return Math.floor(Math.random() * 10) + 1;
 }
 
-console.log(randomDamage());
+/* 
+function chooseOption(opt1, opt2){
+
+} 
+*/
+
+/*
+function attackPlayer(health){
+    return health - randomDamage;
+}
+*/
+
+/*
+function logHealth(player, health){
+    console.log(`${player} health: ${health}`)
+}
+*/
+
+/*
+function logDeath(winner, loser){
+    console.log(`${winner} defeated ${loser}`)
+}
+*/
+
+/*
+function isDead(health){
+    if (health <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+*/
+
+function fight(player1, player2, player1Health, player2Health){
+    
+}
